dc6d91eebaa3ca8b41052f0a3d4b274747b2e825
blackwidow0000 <125404090+blackwindow0000@users.noreply.github.com>
2023-05-24 15:53:12 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,27 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Olá Mundo!");
+    int num = 7;
+    System.out.println(num);
+    int myNum = -3;
+    float myFloatNum = 389.543f;
+    char myLetter = 'G';
+    boolean myBool = true;
+    String myText = "Olá";
+    System.out.println(myNum);
+    System.out.println(myFloatNum);
+    System.out.println(myLetter);
+    System.out.println(myText);
+    System.out.println(myBool);
+    //parte dois do código
+    System.out.println(myText + myBool);
+    int x = 5;
+    float y = x;
+    System.out.println(x);
+    System.out.println(y);
+    double myDouble = 9.78d;
+    int myInt = (int) myDouble;
+    System.out.println(myDouble);
+    System.out.println(myInt);
+  }
+}
