51235d834cb87fd3cf1fa74b65a6c63a8c87cb4f
blackwidow0000 <125404090+blackwindow0000@users.noreply.github.com>
2023-05-29 16:19:10 -0300

    Add files via upload

---
+++ contas.kt
@@ -0,0 +1,20 @@
+fun main() {  
+  var x = 12
+  var y = 3
+  
+  var soma=x+y
+  println("A soma é:"+soma)  
+  
+  var sub=x-y
+  println("A subtração é :"+sub)
+  
+  var divisao=x/y
+  println("A divisão é:"+divisao)
+
+  var mult=x*y
+  println("A multiplicação é:"+mult)
+
+  var por=x%y
+  println(por)
+  
+}                         
