6536791dd040650ed5f7e99e18f0834049e726d8
blackwidow0000 <125404090+blackwindow0000@users.noreply.github.com>
2023-06-19 16:00:08 -0300

    Create inputk.kt

---
+++ inputk.kt
@@ -0,0 +1,28 @@
+fun main(args: Array<String>) {
+
+  println("Qual é seu nome?")
+  var texto = readLine()!!
+  println("Seja bem vinda " + texto)
+  // Faça um programa em kotlin que receba dois numeros inteiros e o programa faz a soma dos numeros
+
+  println("Digite um numero inteiro")
+  var num1 = readLine()!!
+  var x = num1.toInt()
+  println("Digite outro numero inteiro: ")
+  var num2 = readLine()!!
+  var y = num2.toInt()
+  println("Primeiro numero: " + num1)
+  println("Segundo numero: " + num2)
+  var soma = x + y
+  println("O resultado é: " + soma)
+  println("CONTA DE SUBTRAÇÃO") println ("CONTA DE SUBTRAÇÃO") println ("Digite um numero inteiro")
+  var num1 = readLine()!!
+  var x = num1.toInt()
+  println("Digite outro numero inteiro: ")
+  var num2 = readLine()!!
+  var y = num2.toInt()
+  println("Primeiro numero: " + num1)
+  println("Segundo numero: " + num2)
+  var soma = x + y
+  println("O resultado é: " + soma)
+}
